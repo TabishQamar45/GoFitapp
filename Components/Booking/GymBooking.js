53970edd062bec48aceb69a46a17f65252016268
talhamujahid52 <55655696+talhamujahid52@users.noreply.github.com>
2022-10-21 12:55:03 +0500

    Booking Screen Added.

--- Components/Booking/GymBooking.js
+++ Components/Booking/GymBooking.js
@@ -1,14 +1,121 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+import useDynamicStyling from "../../customhooks/useDynamicStyling";
+import DropDownPicker from "react-native-dropdown-picker";
+import { Calendar } from "react-native-calendars";
+import moment from "moment";
 
 const GymBooking = () => {
+  const { DynamicStyles } = useDynamicStyling();
+  const [selected, setSelected] = useState(moment().format("YYYY-MM-DD"));
+  const [openTrainer, setOpenTrainer] = useState(false);
+  const [value, setValue] = useState(null);
+  const [items, setItems] = useState([
+    { label: "Apple", value: "apple" },
+    { label: "Banana", value: "banana" },
+  ]);
+  //
+  const [openTimeslot, setOpenTimeslot] = useState(false);
+
   return (
-    <View>
-      <Text>GymBooking</Text>
-    </View>
-  )
-}
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <Text style={{ color: "white", fontWeight: "bold", marginBottom: 10 }}>
+        Available Trainers
+      </Text>
+      <DropDownPicker
+        placeholder="Select Trainer"
+        dropDownContainerStyle={{
+          backgroundColor: "#292d3e",
+        }}
+        style={{
+          backgroundColor: "#292d3e",
+        }}
+        textStyle={{
+          color: "white",
+        }}
+        listMode="SCROLLVIEW"
+        open={openTrainer}
+        value={value}
+        items={items}
+        setOpen={setOpenTrainer}
+        setValue={setValue}
+        setItems={setItems}
+      />
+      <Calendar
+        minDate={moment().subtract(40, "days").calendar()}
+        maxDate={moment().add(10, "days").calendar()}
+        onDayPress={(day) => {
+          console.log(day.dateString);
+          setSelected(day.dateString);
+        }}
+        markedDates={{
+          [selected]: {
+            selected: true,
+            disableTouchEvent: true,
+            selectedColor: "#B0C929",
+            selectedTextColor: "white",
+            dotColor: "white",
+            marked: true,
+          },
+        }}
+        theme={{
+          arrowColor: "#B0C929",
+          todayTextColor: "#B0C929",
+        }}
+        style={{
+          borderRadius: DynamicStyles(5),
+          marginVertical: DynamicStyles(30),
+        }}
+      ></Calendar>
+      <Text style={{ color: "white", fontWeight: "bold", marginBottom: 10 }}>
+        Available Time Slots
+      </Text>
+      <DropDownPicker
+        placeholder="Select Time"
+        dropDownContainerStyle={{
+          backgroundColor: "#292d3e",
+        }}
+        style={{
+          backgroundColor: "#292d3e",
+        }}
+        textStyle={{
+          color: "white",
+        }}
+        listMode="SCROLLVIEW"
+        open={openTimeslot}
+        value={value}
+        items={items}
+        setOpen={setOpenTimeslot}
+        setValue={setValue}
+        setItems={setItems}
+      />
+      <TouchableOpacity
+        style={{
+          height: 60,
+          width: "100%",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#B0C929",
+          borderRadius: 10,
+          marginTop: 20,
+          marginBottom: 40,
+        }}
+      >
+        <Text style={{ color: "white", fontWeight: "bold" }}>
+          Confirm Appointment
+        </Text>
+      </TouchableOpacity>
+    </ScrollView>
+  );
+};
 
-export default GymBooking
+export default GymBooking;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- screens/HomeActionMenuScreens/BookingScreen.js
+++ screens/HomeActionMenuScreens/BookingScreen.js
@@ -1,14 +1,70 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  SafeAreaView,
+  Dimensions,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import GymBooking from "../../Components/Booking/GymBooking";
+import OnlineBooking from "../../Components/Booking/OnlineBooking";
 
-const BookingScreen = () => {
+const Tab = createMaterialTopTabNavigator();
+const SCREEN_HEIGHT = Dimensions.get("window").height;
+const SCREEN_WIDTH = Dimensions.get("window").width;
+
+const BookScreen = () => {
+  const insets = useSafeAreaInsets();
+
   return (
-    <View>
-      <Text>BookingScreen</Text>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#000",
+        paddingHorizontal: 20,
+      }}
+    >
+      <View
+        style={{
+          paddingTop: 13,
+          paddingBottom: 10,
+          borderBottomWidth: 1,
+          borderBottomColor: "white",
+          marginBottom: 11,
+        }}
+      >
+        <Text style={{ color: "white", fontSize: 28, fontWeight: "400" }}>
+          Book Appointment
+        </Text>
+      </View>
+      <Tab.Navigator
+        sceneContainerStyle={{ backgroundColor: "transparent" }}
+        screenOptions={{
+          tabBarLabelStyle: {
+            color: "white",
+            fontWeight: "600",
+          },
+          // tabBarItemStyle: { width: "100%" },
+          tabBarStyle: { marginBottom: 20, backgroundColor: "#000" },
+          tabBarIndicatorStyle: {
+            height: "100%",
+            borderRadius: 10,
+            backgroundColor: "#B0C929",
+          },
+        }}
+      >
+        <Tab.Screen name="Gym Session" component={GymBooking} />
+        <Tab.Screen name="Online Session" component={OnlineBooking} />
+      </Tab.Navigator>
     </View>
   );
 };
 
-export default BookingScreen;
+export default BookScreen;
 
 const styles = StyleSheet.create({});
